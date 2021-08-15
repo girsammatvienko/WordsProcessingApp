a19ef28a20dc6435ec0b556c6526bf523c68a359
GtName <girsammatvienko@gmail.com>
2021-08-16 00:50:59 +0300

    initial

--- src/test/java/com/example/wordsprocessingapp/WordsProcessingAppApplicationTests.java
+++ src/test/java/com/example/wordsprocessingapp/WordsProcessingAppApplicationTests.java
@@ -4,7 +4,6 @@ import com.example.wordsprocessingapp.controllers.api.MainApiController;
 import com.example.wordsprocessingapp.controllers.gui.MainGuiController;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
