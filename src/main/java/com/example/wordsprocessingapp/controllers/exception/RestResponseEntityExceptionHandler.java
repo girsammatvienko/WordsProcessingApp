88ff8a82e493930fb26e145c7a34df5a7647921a
GtName <girsammatvienko@gmail.com>
2021-08-17 14:20:20 +0300

    17.08

--- src/main/java/com/example/wordsprocessingapp/controllers/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/wordsprocessingapp/controllers/exception/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.example.wordsprocessingapp.controllers.exception;
 
-import com.example.wordsprocessingapp.entities.Errors.CustomError;
+import com.example.wordsprocessingapp.entities.errors.CustomError;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/example/wordsprocessingapp/entities/errors/CustomError.java
+++ src/main/java/com/example/wordsprocessingapp/entities/errors/CustomError.java
@@ -1,11 +1,9 @@
-package com.example.wordsprocessingapp.entities.Errors;
+package com.example.wordsprocessingapp.entities.errors;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
 @NoArgsConstructor
